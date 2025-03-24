7aa69e2315815c2e06e05eb780a2af8a6e285b39
subin5537 <subinp5537@gmail.com>
2025-03-25 01:36:24 +0900

    Update MySQL connection and login logic

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,13 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+// Mysql 커넥터
+using MySqlConnector;
+
 namespace csLogin
 {
-    /// <summary>
-    /// MainWindow.xaml에 대한 상호 작용 논리
-    /// </summary>
     public partial class MainWindow : Window
     {
+               // Mysql 연결 (문자열 이용)
+        private string connectSql = "server=localhost;database=prj_2025;user=root;password=asf418496";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,7 +30,41 @@ namespace csLogin
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+                    // 유저 입력값을 불러와 Trim을 이용하여 LoginCheck 함수 실행
+            if (LoginCheck(UserIdBox.Text.Trim(), UserPasswordBox.Text.Trim()))
+            {
+                MessageBox.Show("로그인 성공");
+            }
+            else
+            {
+                MessageBox.Show("로그인 실패");
+            }
+        }
 
+               // LoginCheck 함수
+        private bool LoginCheck(string userId, string userPassword)
+        {
+            
+            using (MySqlConnection connect = new MySqlConnection(connectSql))   // Mysql 연결 객체
+            {
+                try // 연결 성공시
+                {   
+                    connect.Open();     // 데이터베이스 연결
+
+                                   // 쿼리문을 이용해 cs_users를 선택하여 user_id와 password 찾기
+                    string query = $"SELECT EXISTS(SELECT 1 FROM cs_users WHERE user_id='{userId}' AND password='{userPassword}')";
+
+                    MySqlCommand command = new MySqlCommand(query, connect);
+
+                                    // 결과 값을 return
+                    return Convert.ToBoolean(command.ExecuteScalar());
+                }
+                catch
+                {
+                                    // 실패시 false
+                    return false;
+                }
+            }
         }
 
         private void UserIdBox_TextChanged(object sender, TextChangedEventArgs e)
